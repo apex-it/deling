3b5cd24bf73b0e4e22a3ccb26073d0e6c72e817b
egilkh <egilkh@gmail.com>
2015-09-22 19:31:09 +0200

    Add gulp tasks. Fixes #1.

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+/* jshint node:true */
+
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    uglifycss = require('gulp-uglifycss');
+
+gulp.task('js', function () {
+  return gulp.src('deling.js')
+    .pipe(concat('deling.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('css', function () {
+  return gulp.src('deling.css')
+    .pipe(concat('deling.min.css'))
+    .pipe(uglifycss())
+    .pipe(gulp.dest('.'));
+});
